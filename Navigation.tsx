a3c74a498843e586e43a3378f840fa90a67d3952
vwhit5 <valeriew2005@gmail.com>
2025-11-23 19:02:50 -0800

    Updated Navigation.tsx: changed all references from 'Strategy & GTM' to 'BILD AI STRATEGY & GTM'

---
+++ Navigation.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>BILD AI STRATEGY & GTM</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
