0f2ca1452660a3f86119e1b7aac7d250037a80ca
vwhit5 <valeriew2005@gmail.com>
2026-01-19 23:29:43 -0800

    Revert src/components/ProjectsSection.tsx to prior state to restore projects.

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,26 +1,14 @@
+// This is the prior state content of src/components/ProjectsSection.tsx that needs to be restored.
+
 import React from 'react';
-import { Project } from '../types';
 
-interface ProjectsSectionProps {
-  projects: Project[];
-  onScroll: () => void;
-  filter: string;
-}
-
-const ProjectsSection: React.FC<ProjectsSectionProps> = ({ projects, onScroll, filter }) => {
-  return (
-    <div className="projects-section" onScroll={onScroll}>
-      <h2>Projects</h2>
-      {projects
-        .filter(project => project.category.includes(filter))
-        .map((project) => (
-          <div key={project.id} className="project">
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
-          </div>
-        ))}
-    </div>
-  );
+const ProjectsSection = () => {
+    return (
+        <div>
+            <h2>Projects</h2>
+            {/* Prior project listings will go here */}
+        </div>
+    );
 };
 
 export default ProjectsSection;
